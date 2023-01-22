1b1df0776cbfefdc90fb8fbfd5febcbad333c9a3
kinji2532 <touyamakinji2532@gmail.com>
2023-01-22 20:38:26 +0900

    update

--- src/interactions/index.js
+++ src/interactions/index.js
@@ -9,48 +9,42 @@ const server = app.listen(3000, function(){
 });
 
 app.post("/", async (request, response) => {
-  if (request.method === "POST") {
-    const signature = request.headers["x-signature-ed25519"];
-    const timestamp = request.headers["x-signature-timestamp"];
-    const rawBody = await getRawBody(request);
+  const signature = request.headers["x-signature-ed25519"];
+  const timestamp = request.headers["x-signature-timestamp"];
+  const rawBody = await getRawBody(request);
 
-    const isValidRequest = verifyKey(rawBody, signature, timestamp, process.env.PUBLIC_KEY);
+  const isValidRequest = verifyKey(rawBody, signature, timestamp, process.env.PUBLIC_KEY);
 
-    if (!isValidRequest) return response.status(401).send({ error: "Bad request signature " });
+  if (!isValidRequest) return response.status(401).send({ error: "Bad request signature " });
 
-    const message = request.body;
+  const message = request.body;
 
-    if (message.type === InteractionType.PING) {
-      response.send({ type: InteractionResponseType.PONG });
+  if (message.type === InteractionType.PING) {
+    response.send({ type: InteractionResponseType.PONG });
+  }
+  else if (message.type === InteractionType.APPLICATION_COMMAND) {
+    console.log('APPLICATION_COMMAND', message);
+
+    const commandFunc = cmdList[message.data.name]?.[1];
+
+    if(!commandFunc) return response.status(200).send({ content: "Unknown Command" });
+
+    try {
+      return commandFunc(message, response);
+    } catch(e) {
+      return response.status(200).send({ content: e.message });
     }
-    else if (message.type === InteractionType.APPLICATION_COMMAND) {
-      console.log('APPLICATION_COMMAND', message);
-
-      const commandFunc = cmdList[message.data.name]?.[1];
-
-      if(!commandFunc) return response.status(200).send({ content: "Unknown Command" });
-
-      try {
-        return commandFunc(message, response);
-      } catch(e) {
-        return response.status(200).send({ content: e.message });
-      }
-    } 
-    else if(message.type === InteractionType.APPLICATION_MODAL_SUBMIT) {
-      console.log('APPLICATION_MODAL_SUBMIT', message);
-      const result =response.status(200).send({
-        type: 4,
-        data: { content: 'OK', flags: 64 }
-      });
-      return console.log(result.res);
-    }
-    else {
-      console.log('?', message);
-      response.status(200).send({ content: "Unknown Type" });
-    }
-  } else {
-    let param = {"値":"これはサンプルAPIです"};
-    response.header('Content-Type', 'application/json; charset=utf-8')
-    response.send(param);  
+  } 
+  else if(message.type === InteractionType.APPLICATION_MODAL_SUBMIT) {
+    console.log('APPLICATION_MODAL_SUBMIT', message);
+    const result =response.status(200).send({
+      type: 4,
+      data: { content: 'OK', flags: 64 }
+    });
+    return console.log(result.res);
+  }
+  else {
+    console.log('?', message);
+    response.status(200).send({ content: "Unknown Type" });
   }
 });
